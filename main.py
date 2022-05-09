4b74063af0fad191abf159f595c5450c2b3e0bff
SteveTheSteve1400 <105076133+SteveTheSteve1400@users.noreply.github.com>
2022-05-08 21:50:43 -0400

    Update main.py
    Finished the code for straight, working on turn code

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-
-from microbit import *
-c = 0
-b= 0
-number = 0
-direction = "RIGHT"
-
 def turnL(a: number):
     motobit.enable(MotorPower.ON)
     motobit.set_motor_speed(Motor.LEFT, MotorDirection.REVERSE, 40)
@@ -17,55 +10,61 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    c = input.acceleration(Dimension.X)
-    move(51, 7000)
+    global f
+    f = input.acceleration(Dimension.X)
+    move(7000)
     turnR(590)
-    move(51, 4700)
+    move(4700)
     turnL(590)
-    move(51, 4700)
+    move(4700)
     turnR(590)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-def move(e: number, d: number):
-    while(abs(c - number)>=10):
-            number = input.acceleration(Dimension.X)
-            if(number<=c):
+def move(d: number):
+    c = 0
+    e = 0
+    while e!=d:
+        number2 = input.acceleration(Dimension.X)
+        if number2 <= c:
+            while(abs(c - number2) >= 10):
                 motobit.enable(MotorPower.OFF)
                 motobit.enable(MotorPower.ON)
-                motobit.set_motor_speed(Motor.LEFT, MotorDirection.FORWARD, 5)
-                if(abs(c - number)<=10):
-                    motobit.enable(MotorPower.OFF)
+                motobit.set_motor_speed(Motor.RIGHT, MotorDirection.FORWARD, 5)
+                if abs(c - number2) >= 10:
                     break
-                    #while loop to make it continuious so it adds on
-            elif(number>=c):
+                    motobit.enable(MotorPower.ON)
+        elif number2 >= c:
+            while(abs(c - number2) >= 10):
                 motobit.enable(MotorPower.OFF)
                 motobit.enable(MotorPower.ON)
                 motobit.set_motor_speed(Motor.RIGHT, MotorDirection.REVERSE, 5)
-                if(abs(c - number)>=10):
-                    motobit.enable(MotorPower.OFF)
+                if abs(c - number2) >= 10:
                     break
-                    #while loop to make it continuious so it adds on
-            
+                    motobit.enable(MotorPower.ON)
+        else:
+            motobit.set_motor_speed(Motor.LEFT, MotorDirection.FORWARD, 64)
+            motobit.set_motor_speed(Motor.RIGHT, MotorDirection.REVERSE, 64)
+            pause(d/5)
+            e= e+d/5
+    
+        
+    
+    # while loop to make it continuious so it adds on
     motobit.enable(MotorPower.ON)
     motobit.set_motor_speed(Motor.LEFT, MotorDirection.FORWARD, 64)
-    motobit.set_motor_speed(Motor.RIGHT, MotorDirection.REVERSE, e)
+    motobit.set_motor_speed(Motor.RIGHT, MotorDirection.REVERSE, 64)
     pause(d)
     motobit.enable(MotorPower.OFF)
-    
 def turnR(b: number):
     motobit.enable(MotorPower.ON)
     motobit.set_motor_speed(Motor.LEFT, MotorDirection.FORWARD, 40)
     motobit.set_motor_speed(Motor.RIGHT, MotorDirection.FORWARD, 40)
     motobit.enable(MotorPower.OFF)
+f = 0
+g = 0
+number = 0
+direction = "RIGHT"
 
 def on_forever():
     pause(10000)
-    
-    
-
-
 basic.forever(on_forever)
-
-    
-    
-    
